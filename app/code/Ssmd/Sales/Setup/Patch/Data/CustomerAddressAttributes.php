0bc7a467011a1fc4ed16ce8c236255f455cfbfa8
hiteshbabariya <hitesh@skinsolutions.md>
2023-03-08 16:34:14 +0000

    Added most_recently_used_billing and most_recently_used_shipping fields And tracking_numbers & last_shipment_update attributes.

---
+++ app/code/Ssmd/Sales/Setup/Patch/Data/CustomerAddressAttributes.php
@@ -0,0 +1,137 @@
+<?php
+/**
+ * Copyright ©  All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Ssmd\Sales\Setup\Patch\Data;
+
+use Magento\Customer\Model\Customer;
+use Magento\Customer\Setup\CustomerSetup;
+use Magento\Customer\Setup\CustomerSetupFactory;
+use Magento\Eav\Model\Entity\Attribute\Set;
+use Magento\Eav\Model\Entity\Attribute\SetFactory;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Framework\Setup\Patch\PatchRevertableInterface;
+
+class CustomerAddressAttributes implements DataPatchInterface, PatchRevertableInterface
+{
+
+    /**
+     * @var ModuleDataSetupInterface
+     */
+    private $moduleDataSetup;
+    /**
+     * @var CustomerSetup
+     */
+    private $customerSetupFactory;
+    /**
+     * @var SetFactory
+     */
+    private $attributeSetFactory;
+
+    /**
+     * Constructor
+     *
+     * @param ModuleDataSetupInterface $moduleDataSetup
+     * @param CustomerSetupFactory $customerSetupFactory
+     * @param SetFactory $attributeSetFactory
+     */
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup,
+        CustomerSetupFactory $customerSetupFactory,
+        SetFactory $attributeSetFactory
+    ) {
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->customerSetupFactory = $customerSetupFactory;
+        $this->attributeSetFactory = $attributeSetFactory;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function apply()
+    {
+        $this->moduleDataSetup->getConnection()->startSetup();
+        /** @var CustomerSetup $customerSetup */
+        $customerSetup = $this->customerSetupFactory->create(['setup' => $this->moduleDataSetup]);
+        //$customerEntity = $customerSetup->getEavConfig()->getEntityType(Customer::ENTITY);
+
+        $customerSetup->addAttribute(
+            'customer_address',
+            'most_recently_used_shipping',
+            [
+                'label' => 'most_recently_used_shipping',
+                'input' => 'text',
+                'type' => 'static',
+                'source' => '',
+                'required' => false,
+                'visible' => false,
+                'system' => false,
+                'is_used_in_grid' => true,
+                'is_visible_in_grid' => false,
+                'is_filterable_in_grid' => false,
+                'is_searchable_in_grid' => false,
+                'backend' => ''
+            ]
+        );
+
+        $customerSetup->addAttribute(
+            'customer_address',
+            'most_recently_used_billing',
+            [
+                'label' => 'most_recently_used_billing',
+                'input' => 'text',
+                'type' => 'static',
+                'source' => '',
+                'required' => false,
+                'visible' => false,
+                'system' => false,
+                'is_used_in_grid' => true,
+                'is_visible_in_grid' => false,
+                'is_filterable_in_grid' => false,
+                'is_searchable_in_grid' => false,
+                'backend' => ''
+            ]
+        );
+
+        $attribute = $customerSetup->getEavConfig()->getAttribute('customer_address', 'most_recently_used_shipping');
+        $attribute = $customerSetup->getEavConfig()->getAttribute('customer_address', 'most_recently_used_billing');
+        // $attribute->addData([]);
+        $attribute->save();
+
+        $this->moduleDataSetup->getConnection()->endSetup();
+    }
+
+    public function revert()
+    {
+        $this->moduleDataSetup->getConnection()->startSetup();
+        /** @var CustomerSetup $customerSetup */
+        $customerSetup = $this->customerSetupFactory->create(['setup' => $this->moduleDataSetup]);
+        $customerSetup->removeAttribute('customer_address', 'most_recently_used_shipping');
+        $customerSetup->removeAttribute('customer_address', 'most_recently_used_billing');
+
+        $this->moduleDataSetup->getConnection()->endSetup();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAliases()
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getDependencies()
+    {
+        return [
+
+        ];
+    }
+}
+
